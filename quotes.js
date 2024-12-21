0592a540e7d14f0b7d52c7ffb637c7efa865b7b4
Dmytro Avramenko <avram2705@gmail.com>
2024-12-21 13:50:25 +0100

    spliting app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,49 @@
+const quotes = [
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "Innovation distinguishes between a leader and a follower.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "In the middle of every difficulty lies opportunity.",
+    author: "Albert Einstein",
+  },
+  {
+    quote:
+      "Do not wait to strike till the iron is hot, but make it hot by striking.",
+    author: "William Butler Yeats",
+  },
+  {
+    quote:
+      "It is our choices that show what we truly are, far more than our abilities.",
+    author: "J.K. Rowling",
+  },
+  {
+    quote:
+      "Happiness is not something ready made. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+];
+
+export default quotes;
