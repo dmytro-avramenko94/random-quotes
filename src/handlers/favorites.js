56bb46642f592d5ba19628e17189510f3d847be7
Dmytro Avramenko <avram2705@gmail.com>
2025-01-03 18:18:12 +0100

    Add quote ID to each favorite quote card and remove card from the DOM by id

--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -2,13 +2,12 @@ import { favoriteBtn } from "../../index.js";
 
 function toggleFavorite(quote, btn, container) {
   quote.isFavorite = !quote.isFavorite;
-  const { text, author, isFavorite } = quote;
-  toggleFavoriteBtnIcon(isFavorite, btn);
+  toggleFavoriteBtnIcon(quote.isFavorite, btn);
 
-  if (isFavorite) {
-    showFavoriteCard(text, author, container);
+  if (quote.isFavorite) {
+    showFavoriteCard(quote, container);
   } else {
-    hideFavoriteCard(text);
+    hideFavoriteCard(quote.id);
   }
 }
 
@@ -30,22 +29,27 @@ function hideFavoriteBtn(btn) {
   btn.style.display = "none";
 }
 
-function showFavoriteCard(text, author, container) {
+function showFavoriteCard(quote, container) {
+  const { id, text, author } = quote;
   const favoriteCard = document.createElement("div");
   favoriteCard.classList.add("favorite-card");
+  favoriteCard.dataset.quoteId = id;
   favoriteCard.innerHTML = `
       <p>${text}</p>
       <p class='author'>${author}</p>`;
   container.appendChild(favoriteCard);
 }
 
-function hideFavoriteCard(quote) {
-  const favoriteCards = document.querySelectorAll(".favorite-card");
-  favoriteCards.forEach((card) => {
-    if (card.textContent.includes(quote)) {
-      card.remove();
-    }
-  });
+function hideFavoriteCard(id) {
+  const card = document.querySelector(`.favorite-card[data-quote-id="${id}"]`);
+  if (card) card.remove();
+
+  // const favoriteCards = document.querySelectorAll(".favorite-card");
+  // favoriteCards.forEach((card) => {
+  //   if (card.id === id) {
+  //     card.remove();
+  //   }
+  // });
 }
 
 export { handleFavorite, toggleFavorite, hideFavoriteBtn };
