174327c1ce171bcef6ff43d096155578acdb2591
Dmytro Avramenko <avram2705@gmail.com>
2025-02-13 06:51:03 +0100

    Syncing with local storage

--- index.js
+++ index.js
@@ -1,80 +1,109 @@
-import quotes from "./src/data/quotes.js";
 import {
-  toggleFavorite,
+  toggleFavoriteCard,
   hideFavoriteBtn,
   showFavoriteCard,
+  showFavoriteBtn,
+  removeFavoriteCard,
 } from "./src/handlers/favorites.js";
-import {
-  handleQuote,
-  displayQuote,
-  findQuoteById,
-} from "./src/handlers/quote.js";
+import { displayCurrentQuote } from "./src/handlers/currentQuote.js";
 import {
   localStorageGetItem,
   localStorageSetItem,
 } from "./src/utils/localStorage.js";
+import { getRandomQuote } from "./src/handlers/randomQuote.js";
+import { removeObjectFromArrayById } from "./src/utils/array.js";
 
 const CURRENT_QUOTE_KEY = "currentQuote";
 const FAVORITE_QUOTES_KEY = "favoriteQuotes";
+
+const randomQuoteBtn = document.getElementById("random-quote-btn");
+const quoteFavoriteBtn = document.getElementById("quote-favorite-btn");
+const favoritesContainer = document.getElementById("favorites-container");
+
 let currentQuote = null;
 const favoriteQuotes = [];
 
-function setCurrentQuote(quote, shouldToggleIsFavorite = false) {
-  if (shouldToggleIsFavorite) {
-    quote.isFavorite = !quote.isFavorite;
-    if (quote.isFavorite) {
-      favoriteQuotes.push({ ...quote });
-    } else {
-      const index = favoriteQuotes.findIndex(
-        (favoriteQuote) => favoriteQuote.id === quote.id
-      );
-      if (index !== -1) {
-        favoriteQuotes.splice(index, 1);
-      }
-    }
+function removeFavoriteQuote(id) {
+  // REMOVE FAVORITE QUOTE
+  if (id === currentQuote.id) {
+    // Removing from favorites current quote by clicking on the card Remove from favorites button
+    toggleCurrentQuote();
+  } else {
+    // Removing from favorites quote which is not current
+    // Sync app state by removing favorite quote from the favoriteQuotes array
+    removeObjectFromArrayById(favoriteQuotes, id);
+    // Remove favorite card from UI
+    removeFavoriteCard(id);
+    // Save favorite quotes in the local storage
     localStorageSetItem(FAVORITE_QUOTES_KEY, favoriteQuotes);
   }
-  currentQuote = quote;
+  // // The way to find current quote in the HTML in the other module
+  // const currentQuote = document.querySelector("[data-current-quote-id]");
+  // const currentQuoteId = currentQuote.dataset.currentQuoteId;
+}
+
+function toggleCurrentQuote() {
+  // CURRENT QUOTE UPDATE WHEN LOADED FROM LOCAL STORAGE OR RECEIVED RANDOMLY
+  // sync app state and toggle isFavorite of the current quote
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  // update UI by toggling Icon (no need to display again current quote)
+  showFavoriteBtn(currentQuote.isFavorite);
+  // Save current quote in the local storage
+  localStorageSetItem(CURRENT_QUOTE_KEY, currentQuote);
+
+  // FAVOTITE QUOTES update
+  // sync app state and update favoriteQuotes array
+  if (currentQuote.isFavorite) {
+    favoriteQuotes.push({ ...currentQuote });
+  } else {
+    removeObjectFromArrayById(favoriteQuotes, currentQuote.id);
+  }
+  // update UI by adding or removing favorite card
+  toggleFavoriteCard(currentQuote, favoritesContainer);
+  // Save favorite quotes in the local storage
+  localStorageSetItem(FAVORITE_QUOTES_KEY, favoriteQuotes);
+}
+
+function setCurrentQuote(quote) {
+  // NEW RANDOM QUOTE
+  // Change app state and write copy of the quote to the current quote
+  currentQuote = { ...quote };
+  // Check if id of the current quote is among favorite quotes and set isFavorite to true.
+  currentQuote.isFavorite = !!favoriteQuotes.find(
+    (favoriteQuote) => favoriteQuote.id === currentQuote.id
+  );
+  // show current quote in the UI
+  displayCurrentQuote(currentQuote);
+  // Display favorite icon and change it's state
+  showFavoriteBtn(currentQuote.isFavorite);
+  // Save current quote in the local storage
   localStorageSetItem(CURRENT_QUOTE_KEY, currentQuote);
 }
 
-const favoritesContainer = document.getElementById("favorites-container");
-const quoteFavoriteBtn = document.getElementById("quote-favorite-btn");
-quoteFavoriteBtn.addEventListener("click", () =>
-  toggleFavorite(
-    currentQuote,
-    setCurrentQuote,
-    quoteFavoriteBtn,
-    favoritesContainer
-  )
-);
-
 hideFavoriteBtn();
+quoteFavoriteBtn.addEventListener("click", toggleCurrentQuote);
 
-const generateBtn = document.getElementById("generate-btn");
-generateBtn.addEventListener("click", () =>
-  handleQuote(quotes, favoriteQuotes, setCurrentQuote)
+// expecting new random quote {id, text, author}
+randomQuoteBtn.addEventListener("click", () =>
+  setCurrentQuote(getRandomQuote())
 );
 
 function init() {
-  const currentQuoteFromStorage = localStorageGetItem(CURRENT_QUOTE_KEY);
-  if (currentQuoteFromStorage) {
-    displayQuote(currentQuoteFromStorage);
-    const quote = findQuoteById(quotes, currentQuoteFromStorage.id);
-    quote.isFavorite = currentQuoteFromStorage.isFavorite;
-    currentQuote = quote;
-  }
-
   const favoriteQuotesFromStorage = localStorageGetItem(FAVORITE_QUOTES_KEY);
 
   if (favoriteQuotesFromStorage) {
     favoriteQuotesFromStorage.forEach((quote) => {
       favoriteQuotes.push(quote);
-      showFavoriteCard(quote, setCurrentQuote, favoritesContainer);
+      showFavoriteCard(quote, favoritesContainer);
     });
+  }
+
+  const currentQuoteFromStorage = localStorageGetItem(CURRENT_QUOTE_KEY);
+  if (currentQuoteFromStorage) {
+    setCurrentQuote(currentQuoteFromStorage);
   }
 }
 
 window.addEventListener("load", init);
 
-export { quoteFavoriteBtn };
+export { quoteFavoriteBtn, removeFavoriteQuote };
---
+++ src/handlers/currentQuote.js
@@ -0,0 +1,11 @@
+function displayCurrentQuote(quote) {
+  const { id, text, author, isFavorite } = quote;
+  const quoteElement = document.getElementById("quote");
+  const quoteTextElement = document.getElementById("quote-text");
+  const quoteAuthorElement = document.getElementById("quote-author");
+  quoteElement.dataset.currentQuoteId = id;
+  quoteTextElement.innerHTML = `"${text}"`;
+  quoteAuthorElement.innerHTML = author;
+}
+
+export { displayCurrentQuote };
